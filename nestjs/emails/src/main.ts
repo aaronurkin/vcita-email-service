eeebc4a525c673023c5d7be4a9f40db3e0e36100
Aaron Urkin <aaron.urkin@1beat.com>
2022-05-24 16:26:58 +0300

    Implement email saving in the NestJS application using the Data Mapper pattern of the TypeORM

--- nestjs/emails/src/main.ts
+++ nestjs/emails/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { EmailModule } from './email.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(

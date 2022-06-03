169e6ff6de98b3fba57b1dd0f40e7c39d177c56c
Aaron Urkin <aaron@jobtestprep.com>
2022-06-03 13:07:17 +0300

    Add create email request consumer

---
+++ dotnet/core31/Consumers/Emails/Core31.Consumers.CreateEmail/CreateEmailRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core31.Consumers.CreateEmail
+{
+    internal class CreateEmailRequestHandler : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ dotnet/core31/Consumers/Emails/Core31.Consumers.CreateEmail/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Core31.Consumers.CreateEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configuration =>
+                {
+                    configuration.AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<CreateEmailRequestHandler>();
+                });
+    }
+}

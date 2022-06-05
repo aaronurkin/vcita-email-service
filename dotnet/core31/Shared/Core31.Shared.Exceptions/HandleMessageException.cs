ae87bcb7a09a17ad6cdd046ac00d99c6375319d2
Aaron Urkin <aaron@jobtestprep.com>
2022-06-05 21:12:24 +0300

    Add custom handle message exception

---
+++ dotnet/core31/Shared/Core31.Shared.Exceptions/HandleMessageException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Core31.Shared.Exceptions
+{
+    public class HandleMessageException : Exception
+    {
+        public HandleMessageException()
+            : base()
+        {
+        }
+
+#nullable enable
+        public HandleMessageException(string? message)
+            : base(message)
+        {
+        }
+
+#nullable enable
+        public HandleMessageException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public HandleMessageException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
